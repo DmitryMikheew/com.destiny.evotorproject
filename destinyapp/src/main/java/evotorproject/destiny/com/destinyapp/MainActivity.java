f60aa53060b4ee5483e14de37158165627260ec0
ShadowMasterNew <pppoooiii@mail.ru>
2017-12-02 12:35:04 +0300

    first commit

---
+++ destinyapp/src/main/java/evotorproject/destiny/com/destinyapp/MainActivity.java
@@ -0,0 +1,13 @@
+package evotorproject.destiny.com.destinyapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
